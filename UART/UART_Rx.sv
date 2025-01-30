1c6e81cf219288eb6263c8d5afeb31bbfab2ddeb
antoniomanawesome <antoniomanawesome@gmail.com>
2025-01-30 15:57:54 -0500

    Update UART_Rx.sv

--- UART/UART_Rx.sv
+++ UART/UART_Rx.sv
@@ -3,7 +3,7 @@ module UART_Rx #(
     // (FPGA clk) / (baud rate) = CLKS_PER_BIT
     // CLKS_PER_BIT = (frequency of i_Clock)/(Frequency of UART)
 ) (
-    input logic         i_Clock, //clock
+    input logic         clk, //FPGA clock
     input logic         i_RX_Serial, //serial data stream coming from computer
     output logic        o_RX_DV, //data valid
     output logic [7:0]  o_RX_Byte //the byte we receive from computer
@@ -14,5 +14,14 @@ localparam logic [2:0] RX_START_BIT = 3'b001;
 localparam logic [2:0] RX_DATA_BITS = 3'b010;
 localparam logic [2:0] RX_STOP_BIT  = 3'b011;
 localparam logic [2:0] CLEANUP      = 3'b100;
+
+always_ff @(posedge clk) begin
+    
+end
+
+
+
+
+
 
 endmodule //UART_Rx
