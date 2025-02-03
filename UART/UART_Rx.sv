dd4798caa1fc91344183690869f48877dce772a7
antoniomanawesome <antoniomanawesome@gmail.com>
2025-02-03 16:58:42 -0500

    First iteration of UART Rx
    Watched NANDLAND's video, followed along but will change names and code to clean it up

--- UART/UART_Rx.sv
+++ UART/UART_Rx.sv
@@ -9,19 +9,80 @@ module UART_Rx #(
     output logic [7:0]  o_RX_Byte //the byte we receive from computer
 );
     
-localparam logic [2:0] IDLE         = 3'b000;
-localparam logic [2:0] RX_START_BIT = 3'b001;
-localparam logic [2:0] RX_DATA_BITS = 3'b010;
-localparam logic [2:0] RX_STOP_BIT  = 3'b011;
-localparam logic [2:0] CLEANUP      = 3'b100;
+typedef enum logic [2:0] {
+    IDLE,
+    RX_START_BIT,
+    RX_DATA_BITS,
+    RX_STOP_BIT,
+    CLEANUP
+} state_t;
 
-always_ff @(posedge clk) begin
-    
+state_t state_r;
+
+logic [7:0] r_Clock_Count = '0;
+logic [2:0] r_Bit_Index = '0;
+logic [7:0] r_RX_Byte = '0;
+logic       r_RX_DV = 1'b0;
+logic [2:0] r_SM_Main = '0;
+
+
+
+always @(posedge clk) begin
+    case(state_r)
+        IDLE: begin //Data line is held high during IDLE, so we stay here until a low is received from the line
+            r_RX_DV <= 1'b0;
+            r_Clock_Count <= '0;
+            r_Bit_Index <= '0;
+
+            if(i_RX_Serial == 1'b0) state_r <= RX_START_BIT;
+        end //IDLE
+
+        RX_START_BIT: begin //When line is pulled low, check middle of start bit to make sure it's still low
+            if(r_Clock_Count == (CLKS_PER_BIT-1)/2) begin
+                if(i_RX_Serial == 1'b0) begin
+                    r_Clock_Count <= '0; //reset counter because we found the middle of the start bit
+                    state_r <= RX_DATA_BITS;
+                end else state_r <= IDLE; //line is high, go back to IDLE
+            end else begin
+                r_Clock_Count <= r_Clock_Count + 1;
+            end
+        end //RX_START_BIT
+
+        RX_DATA_BITS: begin //Found the start bit, so now we are waiting for the data bits
+            if(r_Clock_Count < CLKS_PER_BIT-1) begin
+                r_Clock_Count <= r_Clock_Count + 1;
+            end else begin
+                r_Clock_Count <= '0;
+                r_RX_Byte[r_Bit_Index] <= i_RX_Serial;
+
+                //check if we received all of the bits
+                if(r_Bit_Index < 7) begin
+                    r_Bit_Index <= r_Bit_Index + 1;
+                end else begin
+                    r_Bit_Index <= '0;
+                    state_r <= RX_STOP_BIT
+                end
+            end
+        end //RX_DATA_BITS
+
+        RX_STOP_BIT: begin
+            if(r_Clock_Count < CLKS_PER_BIT-1) begin
+                r_Clock_Count <= r_Clock_Count + 1;
+            end else begin
+                r_RX_DV <= 1'b1;
+                r_Clock_Count <= '0;
+                state_r <= CLEANUP;
+            end
+        end
+
+        CLEANUP: begin //stay here 1 clock
+            r_RX_DV <= 1'b0;
+            state_r <= IDLE;
+        end
+
+    endcase;
+
 end
-
-
-
-
 
 
 endmodule //UART_Rx
