43dd15a2f7a7ddaa9f834c23cd5d3f423c4710f9
antoniomanawesome <antoniomanawesome@gmail.com>
2025-02-04 19:09:39 -0500

    Finish ESP32 code to talk to DE10

--- UART/UART_on_ESP32/UART_on_ESP32/src/main.cpp
+++ UART/UART_on_ESP32/UART_on_ESP32/src/main.cpp
@@ -1,11 +1,21 @@
 #include <Arduino.h>
 
-
+String sendMessage = "";
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);    // Initialize the Serial monitor for console debug
+  Serial2.begin(115200);   // Initialize Serial2 for sending data to FPGA
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (Serial.available() > 0) {
+    char inputChar = Serial.read();
+    if (inputChar == '\n') {
+      Serial2.println(sendMessage);  // Send the message through Serial2 with a newline character
+      sendMessage = "";  // Reset the message
+    } else {
+      sendMessage += inputChar;  // Append characters to the message
+    }
+  }
 }
