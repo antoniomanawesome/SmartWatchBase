fc00e0167dc0da712d0a8aba2f3ae6d1619be63d
antoniomanawesome <antoniomanawesome@gmail.com>
2025-02-10 22:45:34 -0500

    ESP32 TO FPGA COMMUNICATION WORKS

--- UART/UART_on_ESP32/UART_on_ESP32/src/main.cpp
+++ UART/UART_on_ESP32/UART_on_ESP32/src/main.cpp
@@ -1,25 +1,31 @@
 #include <Arduino.h>
-
 String sendMessage = "";
+char inputChar = '0';
 void setup() {
-  pinMode(TX, OUTPUT); //setting UART0 Tx line to output 
+  pinMode(TX2, OUTPUT); //setting UART2 Tx line to output (GPIO 17)
   Serial.begin(115200); // Initialize the Serial monitor for console debug
-  Serial2.begin(115200, SERIAL_8N1); // Initialize Serial2 for sending data to FPGA
+  Serial2.begin(115200, SERIAL_8N1); // Initialize Serial2 for sending data to FPGA (1 start bit, 1 stop bit, 8 data bits, no parity)
   //configure uart and experiment with serial write function
   
 }
 
 void loop() {
 
-  if (Serial.available() > 0) {
-    char inputChar = Serial.read();
-    if (inputChar == '\n') {
-      Serial2.println(sendMessage);  // Send the message through Serial2 with a newline character
+  if (Serial.available() > 0) { //if there is a character on the console, run this
+    inputChar = Serial.read(); //reading the char and storing into inputChar
+    if (inputChar == '\n') { //if enter is pressed, send char through UART2
+      Serial2.write(sendMessage[0]);
+      Serial.println(sendMessage[0]); //print to console for debug
+      //Serial2.print(sendMessage);  // Send the message through Serial2 with a newline character
       sendMessage = "";  // Reset the message
-    } else {
-      sendMessage += inputChar;  // Append characters to the message
+    } 
+    else{
+      sendMessage += inputChar;
     }
-    Serial.println(sendMessage);
-    //delay(1000);
   }
 }
+
+/*
+sending lowercase D
+d = 0x64 = 0b01100100
+*/
--- UART/UART_toplevel.sv
+++ UART/UART_toplevel.sv
@@ -1,30 +1,34 @@
-module UART_toplevel(
+module UART_toplevel (
     input logic         clk,
     input logic         RX_Serial,
     output logic        RX_DV,
     output logic [6:0]  SSG1,
-    output logic [6:0]  SSG2
+    output logic [6:0]  SSG2,
+    output logic [7:0] RX_Byte
 );
 
-localparam logic [7:0] RX_Byte = '0;
+//logic [7:0] RX_Byte;
+logic data_valid;
+
+assign RX_DV = ~data_valid;
 
 UART_Rx #(
     .CLKS_PER_BIT()) UART_RX_INST_l
     (
         .clk(clk),
         .i_RX_Serial(RX_Serial),
-        .o_RX_DV(RX_DV),
+        .o_RX_DV(data_valid),
         .o_RX_Byte(RX_Byte)
     );
 
 BCD_conv SevenSeg0_l(
     .number_in(RX_Byte[7:4]),
-    .number_out(SSG1)
+    .number_out(SSG2)
 );
 
 BCD_conv SevenSeg1_l(
     .number_in(RX_Byte[3:0]),
-    .number_out(SSG2)
+    .number_out(SSG1)
 );
 
 endmodule
