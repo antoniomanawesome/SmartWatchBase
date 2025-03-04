92438a622e4397b24593f48c3a3c32148c7be581
antoniomanawesome <antoniomanawesome@gmail.com>
2025-03-04 11:15:59 -0500

    Simulated on Desktop and created SPI_top level

--- SPI/SPI_toplevel.sv
+++ SPI/SPI_toplevel.sv
@@ -1,9 +1,12 @@
 module SPI_toplevel (
     input logic         clk,
     input logic         rst,
-    
+
 );
 
+SPI_Master_w_CS #() SPI_l (
+
+);
 
 BCD_conv SevenSeg0_l(
     .number_in(RX_Byte[7:4]),
