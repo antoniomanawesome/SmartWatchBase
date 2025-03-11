e552c928a8bceb7fd0b5c480d7e56bf13837c51b
antoniomanawesome <antoniomanawesome@gmail.com>
2025-03-10 21:16:14 -0400

    Updating testing.cpp

--- SPI/SPI_on_ESP32/SPI_on_ESP32/src/testing.cpp
+++ SPI/SPI_on_ESP32/SPI_on_ESP32/src/testing.cpp
@@ -16,6 +16,8 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
   OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
+uint8_t data[] = {9, 4, 5, 7, 2, 5, 1, 0, 1, 3, 2, 5, 5, 0};
+
 void testdrawstyles(void);
 
 void setup() {
@@ -33,18 +35,19 @@ void setup() {
     delay(2000); // Pause for 2 seconds
   
     // Clear the buffer
-    display.clearDisplay();
+    // display.clearDisplay();
   
     // Draw a single pixel in white
-    display.drawPixel(10, 10, SSD1306_WHITE);
+    // display.drawPixel(10, 10, SSD1306_WHITE);
   
     // Show the display buffer on the screen. You MUST call display() after
     // drawing commands to make them visible on screen!
-    display.display();
+    // display.display();
     delay(2000);
 }
 
 void loop(){
+    //display time, temperature, pressure, and humidity
     testdrawstyles();
 }
 
@@ -54,14 +57,20 @@ void testdrawstyles(void) {
     display.setTextSize(1);             // Normal 1:1 pixel scale
     display.setTextColor(SSD1306_WHITE);        // Draw white text
     display.setCursor(0,0);             // Start at top-left corner
-    display.println(F("The time is 9:18 PM"));
+    
+    display.print(F("The time is: "));
+    display.print(data[0]);
+    display.println(F(":00 PM"));
+    display.println(F("Temperature: 72.5 F"));
+    display.println(F("Pressure: 1013.25 hPa"));
+    display.println(F("Humidity: 50%"));
   
-    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
-    display.println(3.141592);
+    // display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+    // display.println(3.141592);
   
     //display.setTextSize(2);             // Draw 2X-scale text
-    display.setTextColor(SSD1306_WHITE);
-    display.println(F("Jonathan Gutmann"));
+    // display.setTextColor(SSD1306_WHITE);
+    // display.println(F("Jonathan Gutmann"));
   
     display.display();
     delay(2000);
